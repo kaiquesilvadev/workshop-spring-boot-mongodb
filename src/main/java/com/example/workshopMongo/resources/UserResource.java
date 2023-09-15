a0c6b6c0f8e0c4710c131d9bc526fbd7b2176d4a
kaiquesilvadev <j.kaique1570@gmail.com>
2023-09-15 20:15:36 -0300

    obtendo usuario por id

--- src/main/java/com/example/workshopMongo/resources/UserResource.java
+++ src/main/java/com/example/workshopMongo/resources/UserResource.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -21,16 +22,20 @@ public class UserResource {
 
 	@Autowired
 	private UserService Service;
-	
-	
+
 	@GetMapping
 	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = Service.findAll();
 		List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
-		if(list.isEmpty()) 
+		if (list.isEmpty())
 			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-		
-		return ResponseEntity.ok().body(listDTO
-				);
+
+		return ResponseEntity.ok().body(listDTO);
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		 return Service.findById(id).map(resposta -> ResponseEntity.status(HttpStatus.OK).body(new UserDTO(resposta)))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
 	}
 }
--- src/main/java/com/example/workshopMongo/services/UserService.java
+++ src/main/java/com/example/workshopMongo/services/UserService.java
@@ -1,20 +1,26 @@
 package com.example.workshopMongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.workshopMongo.domain.User;
 import com.example.workshopMongo.repositories.UserRepository;
+import com.example.workshopMongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 
 	@Autowired
 	private UserRepository repository;
-	
+
 	public List<User> findAll() {
 		return repository.findAll();
+	}
+
+	public Optional<User> findById(String id) {
+		return repository.findById(id);
 	}
 }
