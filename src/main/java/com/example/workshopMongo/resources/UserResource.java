bc46c98ddc78abfbb4070f1b32b41f399b008b2b
kaiquesilvadev <j.kaique1570@gmail.com>
2023-09-16 19:18:29 -0300

    método deletar por id

--- src/main/java/com/example/workshopMongo/resources/UserResource.java
+++ src/main/java/com/example/workshopMongo/resources/UserResource.java
@@ -1,20 +1,25 @@
 package com.example.workshopMongo.resources;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.hibernate.internal.build.AllowSysOut;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.example.workshopMongo.domain.User;
 import com.example.workshopMongo.dto.UserDTO;
+import com.example.workshopMongo.repositories.UserRepository;
 import com.example.workshopMongo.services.UserService;
 
 @RestController
@@ -23,6 +28,9 @@ public class UserResource {
 
 	@Autowired
 	private UserService Service;
+	
+	@Autowired
+	private UserRepository repository;
 
 	@GetMapping
 	public ResponseEntity<List<User>> findAll() {
@@ -43,6 +51,14 @@ public class UserResource {
 	public ResponseEntity<UserDTO> insert(@RequestBody UserDTO user) {
 	     User obj = Service.insert(user);
 	     return ResponseEntity.status(HttpStatus.CREATED).body( new UserDTO(obj));
-	  	
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable String id) {
+	    
+	     Service.delete(id);
+	     return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+		
+	     
 	}
 }
--- src/main/java/com/example/workshopMongo/services/UserService.java
+++ src/main/java/com/example/workshopMongo/services/UserService.java
@@ -4,7 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.example.workshopMongo.domain.User;
 import com.example.workshopMongo.dto.UserDTO;
@@ -23,11 +25,20 @@ public class UserService {
 	public Optional<User> findById(String id) {
 		return repository.findById(id);
 	}
-	
+
 	public User insert(UserDTO userDTO) {
 		User user = new User();
 		user.setEmail(userDTO.getEmail());
 		user.setName(userDTO.getName());
 		return repository.insert(user);
+	}
+
+	public void delete(String id) {
+		Optional<User> test = repository.findById(id);
+
+		if (test.isEmpty())
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+		repository.deleteById(id);
 	}
 }
