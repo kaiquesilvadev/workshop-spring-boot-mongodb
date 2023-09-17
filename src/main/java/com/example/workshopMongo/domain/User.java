413e7523d780b180912f1536d1d6eb3ecf9f3609
kaiquesilvadev <j.kaique1570@gmail.com>
2023-09-17 20:38:39 -0300

    referenciando os posts do usuario

--- src/main/java/com/example/workshopMongo/domain/User.java
+++ src/main/java/com/example/workshopMongo/domain/User.java
@@ -1,20 +1,21 @@
 package com.example.workshopMongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @NoArgsConstructor
-@AllArgsConstructor
 @EqualsAndHashCode
 @Getter @Setter
 @Document(collection = "user")
@@ -30,4 +31,13 @@ public class User implements Serializable{
 	@NotBlank
 	@Email
 	private String email;
+	
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
+
+	public User(String id, @NotBlank String name, @NotBlank @Email String email) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+	}
 }
